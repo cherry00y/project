449ac2947b47fa2cc6de66e4e39adf55d3ab7ddb
cherry00y <anuthida.w64@rsu.ac.th>
2024-06-02 20:46:50 +0700

    .

--- API/server.js
+++ API/server.js
@@ -15,6 +15,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3002, () => {
     console.log('CORS-enabled web server listening on port 3001')
 })
