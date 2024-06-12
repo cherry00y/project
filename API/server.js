a890fce41b4f903ae15924bae915168db859c55a
cherry00y <anuthida.w64@rsu.ac.th>
2024-06-12 16:54:39 +0700

    .

--- API/server.js
+++ API/server.js
@@ -150,7 +150,8 @@ app.post('/login', async (req, res) => {
     }
 
     try {
-        const [rows] = await connection.execute('SELECT * FROM admin WHERE username = ? AND password = ?', [username]);
+        // Query to find the user by username
+        const [rows] = await connection.execute('SELECT * FROM admin WHERE username = ?', [username]);
 
         if (rows.length === 0) {
             return res.status(401).json({ message: 'Invalid username or password.' });
@@ -158,12 +159,14 @@ app.post('/login', async (req, res) => {
 
         const user = rows[0];
 
+        // Compare the provided password with the hashed password from the database
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
             return res.status(401).json({ message: 'Invalid username or password.' });
         }
 
+        // Generate JWT
         const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: '1h' });
 
         res.status(200).json({ message: 'Login successful', token });
@@ -172,6 +175,7 @@ app.post('/login', async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+
 
 app.get('/infoadmin', (req, res) => {
     connection.query(
