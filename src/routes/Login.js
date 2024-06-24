f533316e467a661a9c929205cb89dfdce13ae8a6
cherry00y <anuthida.w64@rsu.ac.th>
2024-06-24 21:57:46 +0700

    .

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -22,7 +22,7 @@ function Login() {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:3008/login', {
+            const response = await fetch('http://localhost:3001/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
