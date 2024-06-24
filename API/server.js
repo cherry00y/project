971be42c07eaea6c7d6b55ae1655878bd32b445b
cherry00y <anuthida.w64@rsu.ac.th>
2024-06-24 16:43:26 +0700

    .

--- API/server.js
+++ API/server.js
@@ -47,72 +47,63 @@ app.get('/trivia', (req, res) => {
 });
 
 
-
-
 // เพิ่ม information
 app.post('/information', authenticateToken, (req, res) => {
-    const { title, detail, date, pic, type } = req.body;
+    const { title, detail, date, pic, type,} = req.body;
     const id_admin = req.user.id;
-
-    // ตรวจสอบว่าไม่มีฟิลด์ใดเป็นค่าว่าง
-    if (!title || !detail || !date || !pic || !type) {
-        return res.status(400).send('All fields are required');
-    }
 
     // ดึงชื่อของ admin จากตาราง admin โดยใช้ id_admin
     connection.query('SELECT fname, lname FROM `admin` WHERE id = ?', [id_admin], (err, results) => {
         if (err) {
             console.error('Error in selecting admin:', err);
-            return res.status(500).send('Error selecting admin');
+            res.status(500).send('Error selecting admin');
         } else if (results.length === 0) {
-            return res.status(404).send('Admin not found');
+            res.status(404).send('Admin not found');
         } else {
             const adminName = `${results[0].fname} ${results[0].lname}`;
-            connection.query('INSERT INTO information (title, detail, `date`, pic, `type`, id_admin, created_by, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                [title, detail, date, pic, type, id_admin, adminName, null], (err, results) => {
-                    if (err) {
-                        console.error('Error in POST /information:', err);
-                        return res.status(500).send('Error adding information');
-                    } else {
-                        return res.status(201).send('Information added successfully');
-                    }
-                });
-        }
+            connection.query ('INSERT INTO information (title, detail, `date`, pic, `type`, id_admin, created_by, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [title, detail, date, pic, type, id_admin, adminName, null] ,(err, results) => {
+                if (err) {
+                    console.error('Error in POST /information:', err);
+                    res.status(500).send('Error adding information');
+                } else {
+                    res.status(201).send(results);
+                }
+            });
+        };
     });
 });
-
-
 
 // แก้ไข information
 app.put('/information/:id', authenticateToken, (req, res) => {
     const { id } = req.params;
-    const { title, detail, pic, type } = req.body;
-    const id_admin = req.user.id;  // ดึง id ของผู้ดูแลระบบจาก payload ของโทเค็น
+    const { title, detail, pic, type} = req.body;
+    const id_admin = req.user.id;
     const date = new Date();
 
+    // ดึงชื่อของ admin จากตาราง admin โดยใช้ id_admin
     connection.query('SELECT fname, lname FROM `admin` WHERE id = ?', [id_admin], (err, results) => {
         if (err) {
             console.error('Error in selecting admin:', err);
-            return res.status(500).send('Error selecting admin');
+            res.status(500).send('Error selecting admin');
         } else if (results.length === 0) {
-            return res.status(404).send('Admin not found');
+            res.status(404).send('Admin not found');
         } else {
             const adminName = `${results[0].fname} ${results[0].lname}`;
             connection.query('UPDATE information SET title = ?, detail = ?, `date` = ?, pic = ?, `type` = ?, id_admin = ?, updated_by = ? WHERE id_info = ?',
                 [title, detail, date, pic, type, id_admin, adminName, id],
                 (err, results) => {
                     if (err) {
-                        console.error('Error in PUT /information:', err);
-                        return res.status(500).send('Error updating information');
+                        console.error('Error in PUT /promotion:', err);
+                        res.status(500).send('Error updating information promotion');
                     } else {
-                        return res.status(200).send(results);
+                        res.status(200).send(results);
                     }
                 }
             );
         }
     });
 });
-
 
 
 
@@ -214,8 +205,7 @@ app.post('/login', (req, res) => {
 
 // Middleware สำหรับยืนยันโทเค็น
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = req.headers['authorization'];
 
     if (!token) {
         return res.status(401).send('Access Denied');
@@ -225,12 +215,10 @@ function authenticateToken(req, res, next) {
         if (err) {
             return res.status(403).send('Invalid Token');
         }
-        console.log('Decoded user:', user); // เพิ่มบรรทัดนี้เพื่อตรวจสอบ payload ของโทเค็น
         req.user = user;
         next();
     });
 }
-
 
 app.listen(process.env.PORT || 3010, () => {
     console.log('CORS-enabled web server listening on port 3000')
