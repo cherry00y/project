772b1793658cc20b347d278a6cc5ec8d81dd2d87
cherry00y <anuthida.w64@rsu.ac.th>
2024-06-24 14:56:19 +0700

    .

--- API/server.js
+++ API/server.js
@@ -223,6 +223,6 @@ function authenticateToken(req, res, next) {
     });
 }
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log('CORS-enabled web server listening on port 3000')
 })
