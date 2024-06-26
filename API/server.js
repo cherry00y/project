853d5ce49eb4f27b55a49f3987c66519d00c78e3
cherry00y <anuthida.w64@rsu.ac.th>
2024-06-26 19:15:19 +0700

    .

--- API/server.js
+++ API/server.js
@@ -80,7 +80,7 @@ app.post('/information', authenticateToken, upload.single('pic'), (req, res) => 
             res.status(404).send('Admin not found');
         } else {
             const adminName = `${results[0].fname} ${results[0].lname}`;
-            connection.query('INSERT INTO information (title, detail, `date`, pic, `type`, id_admin, created_by, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', 
+            connection.query('INSERT INTO infor (title, detail, `date`, pic, `type`, id_admin, created_by, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', 
                 [title, detail, date, pic, type, id_admin, adminName, null], (err, results) => {
                 if (err) {
                     console.error('Error in POST /information:', err);
